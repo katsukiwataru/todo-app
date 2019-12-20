c9f1c7731369cf69746e38c733b36bd0f7b46ec7
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-12-20 22:31:35 +0900

    feat: add header

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,11 +4,13 @@ import history from './plugins/history';
 import Home from './container/home';
 import Post from './container/post';
 import PostDesc from './container/postDesc';
+import Header from './components/header';
 
 const App: React.FC = () => {
   return (
     <>
       <Router history={history}>
+        <Header />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/post" component={Post} />
