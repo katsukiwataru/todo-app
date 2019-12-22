7e5329461dd064a3d8e46fab347548e3433ed74d
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-12-23 00:01:11 +0900

    feat: split UI and State

--- src/client/components/homeComp.tsx
+++ src/client/components/homeComp.tsx
@@ -1,55 +1,24 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 // import styled from 'styled-components';
 
-// type Props = {};
+type Props = {
+  product: Products;
+  removeData: (productId: string) => void;
+};
 
-// const Home: React.FC<Props> = () => {
-//   return (
-//     <div>
-//       <ProductTop>
-//         <h1>購入した商品一覧</h1>
-//         <p>
-//           <Link to={`/post/`}>購入した商品の追加</Link>
-//         </p>
-//       </ProductTop>
-//       <Product>
-//         <Name>商品名</Name>
-//         <Price>値段</Price>
-//         <Id>編集</Id>
-//       </Product>
-//     </div>
-//   );
-// };
+const Home: React.FC<Props> = ({ product, removeData }) => {
+  return (
+    <div>
+      <p>{product.title}</p>
+      <p>{product.description}</p>
+      <p>{product.priority}</p>
+      <div>
+        <Link to={`/post/${product.id}`}>編集</Link>
+        <p onClick={() => removeData(product.id as string)}>削除</p>
+      </div>
+    </div>
+  );
+};
 
-// const ProductTop = styled.div`
-//   display: flex;
-//   justify-content: space-between;
-//   width: 50%;
-//   margin: 10px auto;
-//   align-items: center;
-// `;
-
-// const Product = styled.div`
-//   display: flex;
-//   width: 50%;
-//   justify-content: space-between;
-//   text-align: center;
-//   align-items: center;
-//   margin: 10px auto;
-//   background-color: #f6f6f6;
-// `;
-
-// const Name = styled.p`
-//   width: 100%;
-// `;
-
-// const Price = styled.p`
-//   width: 100%;
-// `;
-
-// const Id = styled.p`
-//   width: 100%;
-// `;
-
-// export default Home;
+export default Home;
--- src/client/container/home.tsx
+++ src/client/container/home.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-// import HomeComp from '../components/homeComp';
+import HomeComp from '../components/homeComp';
 import { useQuery, useMutation } from 'react-apollo';
 import gql from 'graphql-tag';
 
@@ -52,30 +51,13 @@ const Home: React.FC = () => {
       {queryError && <Error>ネットワークエラーです。取得できませんでした。</Error>}
       {mutationError && <Error>ネットワークエラーです。削除できませんでした。</Error>}
       {queryData &&
-        queryData.products.map(
-          (product: {
-            id: string | number | undefined;
-            title: string;
-            description: string;
-            priority: number;
-            progress: boolean;
-          }) => {
-            return (
-              <React.Fragment key={product.id}>
-                <div>
-                  <p>{product.title}</p>
-                  <p>{product.description}</p>
-                  <p>{product.priority}</p>
-                  {/* <p>progress{product.progress}</p> */}
-                  <div>
-                    <Link to={`/post/${product.id}`}>編集</Link>
-                    <p onClick={() => removeData(product.id as string)}>削除</p>
-                  </div>
-                </div>
-              </React.Fragment>
-            );
-          },
-        )}
+        queryData.products.map((product) => {
+          return (
+            <React.Fragment key={product.id}>
+              <HomeComp product={product} removeData={removeData} />
+            </React.Fragment>
+          );
+        })}
     </div>
   );
 };
@@ -91,31 +73,5 @@ const Error = styled.div`
   color: red;
   text-align: center;
 `;
-
-// const Product = styled.div`
-//   display: flex;
-//   width: 50%;
-//   justify-content: space-between;
-//   text-align: center;
-//   align-items: center;
-//   margin: 10px auto;
-//   background-color: #f6f6f6;
-// `;
-
-// const Name = styled.p`
-//   width: 100%;
-// `;
-
-// const Price = styled.p`
-//   width: 100%;
-// `;
-
-// const Id = styled.p`
-//   width: 100%;
-// `;
-
-// const Remove = styled.span`
-//   margin: 0 10px;
-// `;
 
 export default Home;
