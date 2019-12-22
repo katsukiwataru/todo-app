d7188bbac4ed3fabce3d72995fd4d0e1fa1c2432
Wataru Katsuki <a17dc050@dhw.ac.jp>
2019-12-23 00:13:29 +0900

    feat: add style

--- src/client/components/homeComp.tsx
+++ src/client/components/homeComp.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 
 type Props = {
   product: Products;
@@ -9,16 +9,29 @@ type Props = {
 
 const Home: React.FC<Props> = ({ product, removeData }) => {
   return (
-    <div>
+    <Contents>
       <p>{product.title}</p>
       <p>{product.description}</p>
       <p>{product.priority}</p>
-      <div>
+      <Change>
         <Link to={`/post/${product.id}`}>編集</Link>
         <p onClick={() => removeData(product.id as string)}>削除</p>
-      </div>
-    </div>
+      </Change>
+    </Contents>
   );
 };
+
+const Contents = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+
+const Change = styled.div`
+  display: flex;
+  align-items: center;
+  width: 10%;
+  justify-content: space-between;
+`;
 
 export default Home;
